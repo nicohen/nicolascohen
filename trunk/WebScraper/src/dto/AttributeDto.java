02f152aaf1c36d6180584a81670a0096b0f89e95
nicohen <nicohen@0b874deb-ba41-0410-971a-5d4381fa7f3e>
2008-08-02 01:30:28 +0000

    Proyecto Web Scraper

---
+++ trunk/WebScraper/src/dto/AttributeDto.java
@@ -0,0 +1,32 @@
+package dto;
+
+public class AttributeDto {
+	private Integer attributeId;
+	private String attributeName;
+	private String attributeValue;
+	
+	public AttributeDto() {}
+	
+	public Integer getAttributeId() {
+		return attributeId;
+	}
+	public void setAttributeId(Integer attributeId) {
+		this.attributeId = attributeId;
+	}
+	public String getAttributeName() {
+		return attributeName;
+	}
+	public void setAttributeName(String attributeName) {
+		this.attributeName = attributeName;
+	}
+	public String getAttributeValue() {
+		return attributeValue;
+	}
+	public void setAttributeValue(String attributeValue) {
+		this.attributeValue = attributeValue;
+	}
+	@Override
+	public String toString() {
+		return "["+attributeName+": "+attributeValue+"]";
+	}
+}
